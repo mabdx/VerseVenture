1d3eb83e4d29322cf2b6b5825e06cd472240c6b2
mabdx <muhammadabdullah9155@gmail.com>
2025-05-20 20:54:21 +0500

    initial

--- backend/index.js
+++ backend/index.js
@@ -22,6 +22,11 @@ app.use((err, req, res, next) => {
     return;
   });
 
+  app.get('/', (req, res) => {
+  res.send('Backend is running!');
+});
+
+
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
